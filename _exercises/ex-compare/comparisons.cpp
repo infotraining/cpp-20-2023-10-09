03d0c3d7b203b2643a9d729bebe899c4bf23d7bb
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-10-09 15:09:05 +0200

    Operator <=> - done

--- _exercises/ex-compare/comparisons.cpp
+++ _exercises/ex-compare/comparisons.cpp
@@ -25,34 +25,49 @@ struct Gadget
     std::string name;
     double price;
 
-    // TODO
+    bool operator==(const Gadget&) const = default;
+    
+    std::strong_ordering operator<=>(const Gadget& other) const
+    {
+        if (auto order = name <=> other.name; order == 0)
+        {
+            return std::strong_order(price, other.price);
+        }
+        else
+            return order;
+    }
 };
 
 struct SuperGadget : Gadget
 {
     Rating rating;
 
-    // TODO
+    std::strong_ordering operator<=>(const SuperGadget&) const = default;
 };
 
 TEST_CASE("Gadget - write custom operator <=> - stronger category than auto detected")
 {
-    // SECTION("==")
-    // {
-    //     CHECK(Gadget{"ipad", 1.0} == Gadget{"ipad", 1.0});
-    // }
+    SECTION("==")
+    {
+        CHECK(Gadget{"ipad", 1.0} == Gadget{"ipad", 1.0});
+    }
     
-    // SECTION("<=>")
-    // {
-    //     CHECK(Gadget{"ipad", 1.0} <=> Gadget{"ipad", 1.0} == std::strong_ordering::equal);
-    // }
+    SECTION("<=>")
+    {
+        auto result = Gadget{"ipad", 1.0} <=> Gadget{"ipad", 1.0};
+        static_assert(std::is_same_v<decltype(result), std::strong_ordering>);
+
+
+        CHECK(result == std::partial_ordering::equivalent);
+        CHECK(result == std::strong_ordering::equal);
+    }
 }
 
-// TEST_CASE("SuperGadget - write custom operator <=> - member without compare-three-way operator")
-// {
-//     CHECK(SuperGadget{{"ipad", 1.0}, Rating{1}} != SuperGadget{{"ipad", 1.0}, Rating{2}});
-//     CHECK(SuperGadget{{"ipad", 1.0}, Rating{1}} <=> SuperGadget{{"ipad", 1.0}, Rating{2}} == std::strong_ordering::less);    
-// }
+TEST_CASE("SuperGadget - write custom operator <=> - member without compare-three-way operator")
+{
+    CHECK(SuperGadget{{"ipad", 1.0}, Rating{1}} != SuperGadget{{"ipad", 1.0}, Rating{2}});
+    CHECK(SuperGadget{{"ipad", 1.0}, Rating{1}} <=> SuperGadget{{"ipad", 1.0}, Rating{2}} == std::strong_ordering::less);    
+}
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -68,14 +83,19 @@ public:
     explicit RatingStar(RatingValue rating_value) : value{rating_value}
     {} 
 
-    // TODO
+    std::strong_ordering operator<=>(const RatingStar&) const = default;
+    
+    std::strong_ordering operator<=>(const RatingValue& other) const
+    {
+        return value <=> other;
+    }
 };
 
-// TEST_CASE("Rating Star - implement nedded <=>")
-// {
-//     RatingStar r1{RatingValue::good};
+TEST_CASE("Rating Star - implement nedded <=>")
+{
+    RatingStar r1{RatingValue::good};
     
-//     CHECK(r1 == RatingStar{RatingValue::good});
-//     CHECK(r1 <=> RatingStar{RatingValue::excellent} == std::strong_ordering::less);
-//     CHECK(r1 <=> RatingValue::excellent == std::strong_ordering::less);
-// }
+    CHECK(r1 == RatingStar{RatingValue::good});
+    CHECK(r1 <=> RatingStar{RatingValue::excellent} == std::strong_ordering::less);
+    CHECK(r1 <=> RatingValue::excellent == std::strong_ordering::less);
+}
